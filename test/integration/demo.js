f554f0e6838e67c1f436014cbc06d045bdb8292a
AstryX <robertasdere@gmail.com>
2019-07-30 13:15:25 +0100

    Changed to demo to show tx creation from raw json.

--- test/integration/demo.js
+++ test/integration/demo.js
@@ -7,6 +7,52 @@ function rng () {
   return Buffer.from('YT8dAtK4d16A3P1z+TpwB2jJ4aFH3g9M1EioIBkLEV4=', 'base64')
 }
 
+const raw = {
+  "version": 2,
+  "flag": 1,
+  "ins": [
+    {
+      "hash": "78fdfddeafc3bac34abe63efee0d64f7d817cee508ded08746ba4ae6df5349cb",
+      "index": 4294967295,
+      "data": "550101",
+      "sequence": 4294967295
+    }
+  ],
+  "outs": [
+    {
+      "value": 0,
+      "data": "6a",
+      "asset": "011b37916db32188c32baa4a7d70ab3ebbe68b6dada8b0ac39333a159eb5cad133",
+      "nonce": "00"
+    },
+    {
+      "value": 0,
+      "data": "6a24aa21a9ed94f15ed3a62165e4a0b99699cc28b48e19cb5bc1b1f47155db62d63f1e047d45",
+      "asset": "011b37916db32188c32baa4a7d70ab3ebbe68b6dada8b0ac39333a159eb5cad133",
+      "nonce": "00"
+    }
+  ],
+  "locktime": 0,
+  "witness_in": [
+    {
+      "issuanceRangeProof": "",
+      "inflationRangeProof": "",
+      "scriptWitness": [ "0000000000000000000000000000000000000000000000000000000000000000" ],
+      "peginWitness": []
+    } 
+  ],
+  "witness_out": [ 
+    {
+      "surjectionProof": "",
+      "rangeProof": ""
+    },
+    {
+      "surjectionProof": "",
+      "rangeProof": ""
+    }
+  ]
+}
+
 // can create a typical p2pkh Transaction --------------------------------------------------------------------------
 const alice1 = ocean.ECPair.makeRandom({ network: gold_main })
 const alice2 = ocean.ECPair.makeRandom({ network: gold_main })
@@ -72,10 +118,10 @@ txb2.addOutput('01e44bd3955e62587468668f367b4702cdcc480454aeedc65c6a3d018e4e61ae
 
 txb2.sign(0, keyPairs[0], p2sh.redeem.output)
 txb2.sign(0, keyPairs[2], p2sh.redeem.output)
-const tx = txb2.build()
+const tx2 = txb2.build()
 
 // The hex can then be used for broadcasting
-const p2shHex = tx.toHex()
+const p2shHex = tx2.toHex()
 
 // At this point the transaction would be broadcast to a node/website like regtest, but we can't use it as ocean is different.
 //await regtestUtils.broadcast(p2shHex)
@@ -101,3 +147,101 @@ console.log("\nWitness In data:")
 console.log(txb4.__TX.witnessIn)
 console.log("\nWitness Out data:")
 console.log(txb4.__TX.witnessOut)
+
+//Example how to construct a transaction from raw JSON + witness
+
+const txwit = new ocean.Transaction()
+txwit.version = raw.version
+txwit.locktime = raw.locktime
+
+if(raw.flag)
+  txwit.flag = raw.flag
+
+raw.ins.forEach((txIn, i) => {
+  const txHash = Buffer.from(txIn.hash, 'hex')
+  let scriptSig
+  let issuance
+
+  if (txIn.data) {
+    scriptSig = Buffer.from(txIn.data, 'hex')
+  } else if (txIn.script) {
+    scriptSig = bscript.fromASM(txIn.script)
+  }
+
+  if (txIn.issuance) {
+    issuance = {
+      assetBlindingNonce: Buffer.from(txIn.issuance.assetBlindingNonce, 'hex'),
+      assetEntropy: Buffer.from(txIn.issuance.assetEntropy, 'hex'),
+      assetamount: Buffer.from(txIn.issuance.assetamount, 'hex'),
+      tokenamount: Buffer.from(txIn.issuance.tokenamount, 'hex')
+    }
+  }
+
+  txwit.addInput(txHash, txIn.index, txIn.sequence, scriptSig, issuance)
+})
+
+raw.outs.forEach(txOut => {
+  let script
+
+  if (txOut.data) {
+    script = Buffer.from(txOut.data, 'hex')
+  } else if (txOut.script) {
+    script = bscript.fromASM(txOut.script)
+  }
+
+  let value = Buffer.from('010000000000000000', 'hex')
+  const numToBuffer = Buffer.alloc(8);
+
+  if (txOut.value) {
+    BufferUtils.writeUInt64LE(numToBuffer, txOut.value, 0)
+    value = Buffer.concat([
+      Buffer.from('01', 'hex'),
+      BufferUtils.reverseBuffer(numToBuffer),
+    ])
+  }
+  else if (txOut.nValue) value = Buffer.from(txOut.nValue, 'hex')
+
+  txwit.addOutput(Buffer.from(txOut.asset, 'hex'), value, Buffer.from(txOut.nonce, 'hex'), script)
+})
+
+witness_in = []
+witness_out = []
+
+if (raw.witness_in) {
+  for (let i = 0; i < raw.witness_in.length; ++i) {
+    scriptWitness = []
+    peginWitness = []
+    for (let j = 0; j < raw.witness_in[i].scriptWitness.length; ++j){
+      scriptWitness.push(Buffer.from(raw.witness_in[i].scriptWitness[j], 'hex'))
+    }
+    for (let j = 0; j < raw.witness_in[i].peginWitness.length; ++j){
+      peginWitness.push(Buffer.from(raw.witness_in[i].peginWitness[j], 'hex'))
+    }
+    witness_in.push(
+      {
+        issuanceRangeProof: Buffer.from(raw.witness_in[i].issuanceRangeProof, 'hex'),
+        inflationRangeProof: Buffer.from(raw.witness_in[i].inflationRangeProof, 'hex'),
+        scriptWitness: scriptWitness,
+        peginWitness: peginWitness
+      }
+    )
+  }
+}
+
+if (raw.witness_out) {
+  for (let i = 0; i < raw.witness_out.length; ++i) {
+    witness_out.push(
+      {
+        surjectionProof: Buffer.from(raw.witness_out[i].surjectionProof, 'hex'),
+        rangeProof: Buffer.from(raw.witness_out[i].rangeProof, 'hex')
+      }
+    )
+  }  
+}
+
+txwit.witnessIn = witness_in
+txwit.witnessOut = witness_out
+
+console.log('Read from hex inflationRangeProof of witness in vs constructed from raw inflationRangeProof of witness in (empty for both):')
+console.log(txb4.__TX.witnessIn[0].inflationRangeProof)
+console.log(txwit.witnessIn[0].inflationRangeProof)
